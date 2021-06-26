8d4516be1a17d7f72f6784e0878dffe15c8c9137
Bogdan Bledea <bledea.bogdan97@gmail.com>
2021-06-26 17:52:19 +0300

    Update Workflows.jsx

--- src/components/pages/Workflows.jsx
+++ src/components/pages/Workflows.jsx
@@ -24,17 +24,12 @@ const Articles = () => {
   return (
     <Table height={400} data={workflows} loading={isLoading}>
       <Table.Column flexGrow={1}>
-        <Table.HeaderCell>Title</Table.HeaderCell>
-        <Table.Cell dataKey='title' />
+        <Table.HeaderCell>Id</Table.HeaderCell>
+        <Table.Cell dataKey='id' />
       </Table.Column>
       <Table.Column flexGrow={1}>
-        <Table.HeaderCell>Published date</Table.HeaderCell>
-        <Table.Cell>
-          {(rowData) => {
-            const date = new Date(rowData.date);
-            return date.toLocaleString('en-GB');
-          }}
-        </Table.Cell>
+        <Table.HeaderCell>Title</Table.HeaderCell>
+        <Table.Cell dataKey='title' />
       </Table.Column>
     </Table>
   );
