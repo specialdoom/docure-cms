495dc3fba104081fb133aa9dfd11fe5092cb406b
Bogdan Bledea <bledea.bogdan97@gmail.com>
2021-06-30 00:11:46 +0300

    Some updates

--- src/config/api.js
+++ src/config/api.js
@@ -1,11 +1,11 @@
 export const apiEndpoints = {
-  users: 'http://localhost:5001/docure-9a8dd/us-central1/docureCMS/users',
+  users: 'http://localhost:3001/docure-9a8dd/us-central1/docureCMS/users',
   article: {
-    add: 'http://localhost:5001/docure-9a8dd/us-central1/docureCMS/article',
-    getAll: 'http://localhost:5001/docure-9a8dd/us-central1/docureCMS/articles',
-    remove: 'http://localhost:5001/docure-9a8dd/us-central1/docureCMS/article'
+    add: 'http://localhost:3001/docure-9a8dd/us-central1/docureCMS/article',
+    getAll: 'http://localhost:3001/docure-9a8dd/us-central1/docureCMS/articles',
+    remove: 'http://localhost:3001/docure-9a8dd/us-central1/docureCMS/article'
   },
   workflow: {
-    getAll: 'http://localhost:5001/docure-9a8dd/us-central1/docureCMS/workflows'
+    getAll: 'http://localhost:3001/docure-9a8dd/us-central1/docureCMS/workflows'
   }
 };
